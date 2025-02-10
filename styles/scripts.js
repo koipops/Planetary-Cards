7d33e296cf05cfbb674812be125e1240179801b2
koipops <lekhoi1@msu.edu>
2025-02-10 00:06:36 +0000

    scripts hopefloly work

---
+++ styles/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    ScrollReveal().reveal('.planet, .sun-card', {
+        origin: 'bottom',
+        distance: '50px',
+        duration: 800,
+        reset: true
+    });
+});
